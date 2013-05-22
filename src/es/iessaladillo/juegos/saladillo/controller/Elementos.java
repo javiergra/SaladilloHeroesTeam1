481b8e18f409a12f6bdd8706d2182a98bf3def24
oskarko <oscar.garrucho@gmail.com>
2013-05-22 11:10:53 +0200

    Picando el Mapa... Óscar

--- src/es/iessaladillo/juegos/saladillo/controller/Elementos.java
+++ src/es/iessaladillo/juegos/saladillo/controller/Elementos.java
@@ -1,43 +1,79 @@
 package es.iessaladillo.juegos.saladillo.controller;
 
-import java.util.ArrayList;
 
 public class Elementos implements Dibujable{
 
-ArrayList<Dibujable> elementos=new ArrayList<Dibujable>();
-
-@Override
-public String getNombreImagen() {
-	// TODO Auto-generated method stub
-	return null;
-}
-
-@Override
-public boolean contieneFondo() {
-	// TODO Auto-generated method stub
-	return false;
-}
-
-@Override
-public Dibujable getFondo() {
-	// TODO Auto-generated method stub
-	return null;
-}
-
-@Override
-public void setFondo(Dibujable dibujable) {
-	// TODO Auto-generated method stub
+	private byte X;
+	private byte Y;
+	private String nombreImagen;
+	private String tipoImagen;
+	private Dibujable fondo;
 	
-}
+	Elementos(byte X, byte Y, String nombreImagen, String tipoImagen, Dibujable fondo){
+		this.X = X;
+		this.Y = Y;
+		this.nombreImagen = nombreImagen;
+		this.tipoImagen = tipoImagen;
+		this.fondo = fondo;
+	}
+	
+	public byte getX() {
+		return X;
+	}
 
-@Override
-public String getNombreClase() {
-	// TODO Auto-generated method stub
-	return null;
-}
-public Object clone() {
-	return null;
-}
+	public void setX(byte x) {
+		X = x;
+	}
+
+	public byte getY() {
+		return Y;
+	}
+
+	public void setY(byte y) {
+		Y = y;
+	}
+
+	@Override
+	public String getNombreImagen() {
+	
+		return nombreImagen;
+	}
+
+	@Override
+	public boolean contieneFondo() {
+		boolean contenido = false;
+	
+		if (fondo != null)
+			contenido = true;
+	
+		return contenido;
+	}
+
+	@Override
+	public Dibujable getFondo() {
+	
+		return fondo;
+	}
+
+	@Override
+	public void setFondo(Dibujable dibujable) {
+		fondo = dibujable;
+	
+	}
+
+	@Override
+	public String getNombreClase() {
+
+		return tipoImagen;
+	}
+	
+	public Object clone() {
+		try{
+			return (super.clone());
+		}catch(CloneNotSupportedException cnse){
+			return null;
+		}
+	}
 
 
 }
--- src/es/iessaladillo/juegos/saladillo/controller/Mapa.java
+++ src/es/iessaladillo/juegos/saladillo/controller/Mapa.java
@@ -1,5 +1,5 @@
 package es.iessaladillo.juegos.saladillo.controller;
-//Esta clase esta mal.XQ? Y por quŽ? Pueba Oscar Moreno
+//Esta clase esta mal.XQ? Y por quï¿½? Pueba Oscar Moreno
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -11,28 +11,39 @@ public class Mapa implements MapaInterface{
 	
 	public void ponerMapa(){
 	ArrayList<Entidad> entidad = null;
-	Elementos elemento = null;
-	Dibujable element;
-	element=elemento;
-	Iterator iterador=entidad.iterator();
-	while (iterador.hasNext()){
-		Posicion posicion= new Posicion (entidad.iterator().next().getX(),entidad.iterator().next().getY());
-		ponerElemento(posicion, element);
+	
 	}
-	}
+	
 	@Override
 	public Dibujable obtenerPosicion(Posicion posicion) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
 	@Override
 	public Posicion getPosicionHeroe() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
 	@Override
 	public void ponerElemento(Posicion posicion, Dibujable dibujable) {
-		// TODO Auto-generated method stub
+		int x, y;
+		Dibujable contenido;
 		
+		x = posicion.getX();
+		y = posicion.getY();
+		
+		if (mapa[x][y] == null)
+			mapa[x][y] = dibujable;
+		else {
+			contenido = mapa[x][y];
+			if (contenido.contieneFondo())
+				dibujable.setFondo(contenido);
+			else if((contenido.getNombreImagen().equals("TELETRANSPORTEAZUL")) ||
+					(contenido.getNombreImagen().equals("TELETRANSPORTEROJO")) )
+				dibujable.setFondo(contenido);
+		}
+			
 	}
 }
