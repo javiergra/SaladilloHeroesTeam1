b40d51706cd3ff40f80a8358fc9258ade0d5f127
oskarko <oscar.garrucho@gmail.com>
2013-05-23 12:13:50 +0200

    CArgarMapaAccion

--- src/es/iessaladillo/juegos/saladillo/Acciones/CargarMapaAccion.java
+++ src/es/iessaladillo/juegos/saladillo/Acciones/CargarMapaAccion.java
@@ -1,10 +1,19 @@
 package es.iessaladillo.juegos.saladillo.Acciones;
 
+import es.iessaladillo.juegos.saladillo.controller.Mapa;
+import es.iessaladillo.juegos.saladillo.util.Entidad;
+
 
 public class CargarMapaAccion implements Accion{
+	
+	Entidad[] entidades;
+	
+	public CargarMapaAccion(Entidad[] entidades){
+		this.entidades = entidades;
+	}
 
 	public  Object execute() {
-		return null;
+		return new Mapa(entidades);
 	}
 
 }
