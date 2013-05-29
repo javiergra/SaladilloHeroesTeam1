1564954e043cbbb6d665a6175f15229b3bad65ad
oskarko <oscar.garrucho@gmail.com>
2013-05-29 10:50:10 +0200

    MoverAccion(), sólo falta PosicionesAActualizarAccion()

--- src/es/iessaladillo/juegos/saladillo/Acciones/MoverAccion.java
+++ src/es/iessaladillo/juegos/saladillo/Acciones/MoverAccion.java
@@ -1,9 +1,42 @@
 package es.iessaladillo.juegos.saladillo.Acciones;
 
-public class MoverAccion implements Accion{
+import es.iessaladillo.juegos.saladillo.controller.Dibujable;
+import es.iessaladillo.juegos.saladillo.controller.Mapa;
+import es.iessaladillo.juegos.saladillo.util.Direccion;
+import es.iessaladillo.juegos.saladillo.util.Posicion;
 
+public class MoverAccion implements Accion{
+	Mapa mapa;
+	Direccion direccion;
+	
+	public MoverAccion(Mapa mapa, Direccion direccion){
+		this.mapa = mapa;
+		this.direccion = direccion;
+	}
+	
 	public  Object execute() {
-		return null;
+		Posicion posicionHeroe, posicion;
+		Dibujable heroe, elemento;
+		
+		posicion = mapa.getPosicionHeroe();
+		heroe = mapa.obtenerPosicion(posicion);
+		if (mapa.sePuedeMover(posicion, direccion, (byte) 0)){
+			
+			elemento = heroe.getFondo();
+			mapa.ponerElemento(posicion, null);
+			mapa.ponerElemento(posicion, elemento);
+		
+			posicionHeroe = mapa.siguientePosicion(posicion, direccion);	
+			elemento =mapa.obtenerPosicion(posicionHeroe);
+			
+			if (elemento.getNombreClase().equals("ForegroundMovil")){	// Si hay pelota...
+				mapa.ponerElemento(posicionHeroe, elemento.getFondo());
+				posicion = mapa.siguientePosicion(posicionHeroe, direccion);
+				mapa.ponerElemento(posicion, elemento);
+			}
+			mapa.ponerElemento(posicionHeroe, heroe);
+		}
+		return mapa;
 	}
 
 }
--- src/es/iessaladillo/juegos/saladillo/facade/SaladilloFacade.java
+++ src/es/iessaladillo/juegos/saladillo/facade/SaladilloFacade.java
@@ -31,30 +31,8 @@ public class SaladilloFacade implements SaladilloFacadeDelegate {
 
 	@Override
 	public MapaInterface mover(Direccion direccion) {
-		Posicion posicionHeroe, posicion;
-		Dibujable heroe, elemento;
-		//A la hora de mover, se podría aprovechar y enviar la posicion antigua del heroe al
-		//ArrayList de ConjuntoPosiciones para el historial de movimiento. Se podría hacer llamando al
-		//metodo posicionesAActualizar()?
-		posicion = mapa.getPosicionHeroe();
-		heroe = mapa.obtenerPosicion(posicion);
-		if (mapa.sePuedeMover(posicion, direccion, (byte) 0)){
-			
-			elemento = heroe.getFondo();
-			mapa.ponerElemento(posicion, null);
-			mapa.ponerElemento(posicion, elemento);
-		
-			posicionHeroe = mapa.siguientePosicion(posicion, direccion);	
-			elemento =mapa.obtenerPosicion(posicionHeroe);
-			
-			if (elemento.getNombreClase().equals("ForegroundMovil")){	// Si hay pelota...
-				mapa.ponerElemento(posicionHeroe, elemento.getFondo());
-				posicion = mapa.siguientePosicion(posicionHeroe, direccion);
-				mapa.ponerElemento(posicion, elemento);
-			}
-			mapa.ponerElemento(posicionHeroe, heroe);
-		}
-		return mapa;
+		Accion accion = new MoverAccion(mapa, direccion);
+		return (MapaInterface) accion.execute();
 	}
 
 	@Override
